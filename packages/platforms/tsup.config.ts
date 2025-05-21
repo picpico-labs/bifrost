4a05ac10a3bed2e79fd435ec2b7c099eab07edaf
Wonhee Lee <2wheeh@gmail.com>
2025-05-22 01:57:51 +0900

    feat: impl simple browser webrtc adapter

--- packages/platforms/tsup.config.ts
+++ packages/platforms/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/machines/index.ts', 'src/adapters/index.ts', 'src/engine/index.ts', 'src/lib/index.ts'],
+  entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   clean: true,
