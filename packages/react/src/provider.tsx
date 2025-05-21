aa802e23b323bf1ae4489d44a9d4eb57b38b1307
Wonhee Lee <2wheeh@gmail.com>
2025-05-21 14:33:51 +0900

    feat: integrate react

---
+++ packages/react/src/provider.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from 'react';
+
+import { getBifrostContext } from './context';
+
+export function BifrostProvider({ children }: PropsWithChildren) {
+  const BifrostContext = getBifrostContext();
+
+  return <BifrostContext.Provider>{children}</BifrostContext.Provider>;
+}
